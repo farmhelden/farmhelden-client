82a3b5ac6f7cba36db56123e57c25410821d04ef
Florian Unzicker <unzico@gmail.com>
2020-03-21 15:39:41 +0100

    Fix build issue due to Fragment name

--- components/Form/components/FormField.tsx
+++ components/Form/components/FormField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 type Props = {
   hasError: boolean;
@@ -12,10 +12,10 @@ type Props = {
  */
 const FormField = ({ hasError, error, children }: Props) => {
   return (
-    <>
+    <Fragment>
       {children}
       {hasError && <div className="mt-1 pl-1 text-red-600">{error}</div>}
-    </>
+    </Fragment>
   );
 };
 
