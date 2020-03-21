f51d2363da777e15ef5970954800c93173d5bc55
Florian Unzicker <unzico@gmail.com>
2020-03-21 01:23:39 +0100

    Add title component

---
+++ components/Title/Title.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+
+type Props = {
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  block?: boolean;
+  bold?: boolean;
+  children: React.ReactNode;
+} & React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLHeadingElement>,
+  HTMLHeadingElement
+>;
+
+const Title = ({
+  as: Component = "h6",
+  block,
+  bold,
+  className,
+  children,
+  ...rest
+}: Props) => {
+  const css = classnames({ block, "font-medium": bold }, className);
+
+  return (
+    <Component className={css} {...rest}>
+      {children}
+    </Component>
+  );
+};
+
+export default Title;
