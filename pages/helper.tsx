4523e0e2211acbbbaee546e6ff61623c28821e87
Florian Unzicker <unzico@gmail.com>
2020-03-21 15:42:07 +0100

    Use custom form instead of material ui

--- pages/helper.tsx
+++ pages/helper.tsx
@@ -1,17 +1,15 @@
 import { useMemo } from "react";
 import { NextPageContext } from "next";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
-import { Container, FormControl } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import Button from "@material-ui/core/Button";
 import { Table } from "../components/Table";
 
 import Header from "../components/Header";
 import Root from "../components/Root";
+import { Formik, FormikProps, Form } from "formik";
+import { PrimaryButton } from "../components/Button";
+import { InputField } from "../components/Form";
 
 function createData(title: string, location: string, neededHelpers: number) {
   return { title, location, neededHelpers };
@@ -24,18 +22,6 @@ const data = [
   createData("Cupcake", "Frankfurt am Main", 12),
   createData("Gingerbread", "Frankfurt am Main", 6)
 ];
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    form: {
-      marginBottom: theme.spacing(2)
-    },
-    gridSubmitButton: {
-      display: "flex",
-      alignItems: "flex-end"
-    }
-  })
-);
 
 function useFarmTableColumns() {
   return useMemo(
@@ -60,8 +46,12 @@ function useFarmTableColumns() {
   );
 }
 
+const initialValues = {
+  location: "",
+  radius: null as number | null
+};
+
 function Helper({ data }: any) {
-  const classes = useStyles();
   const columns = useFarmTableColumns();
 
   return (
@@ -69,42 +59,37 @@ function Helper({ data }: any) {
       <Container>
         <Header />
         {/* Form */}
-        <form
-          noValidate
-          autoComplete="off"
-          className={classes.form}
-          onSubmit={e => {
-            e.preventDefault();
+
+        <Formik
+          initialValues={initialValues}
+          onSubmit={values => {
+            window.alert(JSON.stringify(values, null, 2));
           }}
         >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={5}>
-              <TextField id="location" label="Standort" fullWidth />
-            </Grid>
-            <Grid item xs={12} sm={5}>
-              <FormControl fullWidth>
-                <InputLabel id="radius-label">Radius</InputLabel>
-                <Select labelId="radius-label" id="radius">
-                  <MenuItem value={10}>10 km</MenuItem>
-                  <MenuItem value={20}>20 km</MenuItem>
-                  <MenuItem value={30}>30 km</MenuItem>
-                  <MenuItem value={40}>40 km</MenuItem>
-                  <MenuItem value={50}>50 km</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={2} className={classes.gridSubmitButton}>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Filtern
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
+          {({ values }: FormikProps<typeof initialValues>) => {
+            return (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={5}>
+                    <InputField
+                      name="location"
+                      placeholder="Frankfurt am Main"
+                      block
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={5}>
+                    <InputField name="radius" placeholder="30 km" block />
+                  </Grid>
+                  <Grid item xs={12} sm={2}>
+                    <PrimaryButton type="submit" block>
+                      Filtern
+                    </PrimaryButton>
+                  </Grid>
+                </Grid>
+              </Form>
+            );
+          }}
+        </Formik>
 
         {/* Table */}
         <Table columns={columns} data={data} block />
