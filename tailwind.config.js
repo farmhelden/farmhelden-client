796e4731db311ebc271826b67aeefb9a6be52c50
Florian Unzicker <unzico@gmail.com>
2020-03-21 18:39:10 +0100

    Add and update colors of corporate identity

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     fontFamily: {
       title: ["Roboto Slab, sans-serif"],
       body: ["Roboto, sans-serif"]
+    },
+    extend: {
+      colors: {
+        "primary-light": "#91B169",
+        "primary-dark": "#325802",
+        "secondary-light": "#CEEECE",
+        "secondary-dark": "#749A60",
+        "accent-light": "#F5E5D4",
+        "accent-dark": "#F5E5D4"
+      }
     }
   },
   variants: {
