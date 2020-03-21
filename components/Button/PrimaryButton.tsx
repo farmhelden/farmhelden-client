d408bb65a3c380813b58979a249b32a1d2fe2a99
Florian Unzicker <unzico@gmail.com>
2020-03-21 01:23:27 +0100

    Add button components

---
+++ components/Button/PrimaryButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classnames from "classnames";
+import BaseButton, { Props as BaseButtonProps } from "./BaseButton";
+
+type Props = {
+  theme?: "flat" | "border";
+} & BaseButtonProps;
+
+const PrimaryButton = ({ theme = "flat", className, ...rest }: Props) => {
+  const css = classnames(
+    "border-green-500 hover:border-green-600",
+    {
+      "bg-green-500 hover:bg-green-600 text-white": theme === "flat",
+      "text-green-500 hover:bg-green-600 hover:text-white": theme === "border"
+    },
+    className
+  );
+
+  return <BaseButton className={css} {...rest} />;
+};
+
+export default PrimaryButton;
