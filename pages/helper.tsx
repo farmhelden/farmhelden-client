40f532c6b319cd6ed34fdbe47fe514c1bf5e924e
Florian Unzicker <unzico@gmail.com>
2020-03-21 01:25:32 +0100

    Update pages to reflect css changes

--- pages/helper.tsx
+++ pages/helper.tsx
@@ -16,6 +16,7 @@ import Button from "@material-ui/core/Button";
 
 import Header from "../components/Header";
 import { StyledTableHead } from "../components/Table";
+import Root from "../components/Root";
 
 function createData(title: string, location: string, neededHelpers: number) {
   return { title, location, neededHelpers };
@@ -45,68 +46,74 @@ function Helper({ data }: any) {
   const classes = useStyles();
 
   return (
-    <Container>
-      <Header />
+    <Root>
+      <Container>
+        <Header />
+        {/* Form */}
+        <form
+          noValidate
+          autoComplete="off"
+          className={classes.form}
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={5}>
+              <TextField id="location" label="Standort" fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              <FormControl fullWidth>
+                <InputLabel id="radius-label">Radius</InputLabel>
+                <Select labelId="radius-label" id="radius">
+                  <MenuItem value={10}>10 km</MenuItem>
+                  <MenuItem value={20}>20 km</MenuItem>
+                  <MenuItem value={30}>30 km</MenuItem>
+                  <MenuItem value={40}>40 km</MenuItem>
+                  <MenuItem value={50}>50 km</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={2} className={classes.gridSubmitButton}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Filtern
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
 
-      {/* Form */}
-      <form
-        noValidate
-        autoComplete="off"
-        className={classes.form}
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={5}>
-            <TextField id="location" label="Standort" fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={5}>
-            <FormControl fullWidth>
-              <InputLabel id="radius-label">Radius</InputLabel>
-              <Select labelId="radius-label" id="radius">
-                <MenuItem value={10}>10 km</MenuItem>
-                <MenuItem value={20}>20 km</MenuItem>
-                <MenuItem value={30}>30 km</MenuItem>
-                <MenuItem value={40}>40 km</MenuItem>
-                <MenuItem value={50}>50 km</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={2} className={classes.gridSubmitButton}>
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-              Filtern
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-
-      {/* Table */}
-      <TableContainer component={Paper}>
-        <Table aria-label="Derzeitige Ernten" size="small">
-          <StyledTableHead>
-            <TableRow>
-              <TableCell>Ernteart</TableCell>
-              <TableCell>Standort</TableCell>
-              <TableCell>Benötigte Helfer</TableCell>
-              <TableCell>Aktion</TableCell>
-            </TableRow>
-          </StyledTableHead>
-          <TableBody>
-            {data.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.title}
-                </TableCell>
-                <TableCell>{row.location}</TableCell>
-                <TableCell>{row.neededHelpers}</TableCell>
-                <TableCell>Details anzeigen</TableCell>
+        {/* Table */}
+        <TableContainer component={Paper}>
+          <Table aria-label="Derzeitige Ernten" size="small">
+            <StyledTableHead>
+              <TableRow>
+                <TableCell>Ernteart</TableCell>
+                <TableCell>Standort</TableCell>
+                <TableCell>Benötigte Helfer</TableCell>
+                <TableCell>Aktion</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+            </StyledTableHead>
+            <TableBody>
+              {data.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.title}
+                  </TableCell>
+                  <TableCell>{row.location}</TableCell>
+                  <TableCell>{row.neededHelpers}</TableCell>
+                  <TableCell>Details anzeigen</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </Root>
   );
 }
 
