aee46ff37dd7578fe0d1908c40ab0ea406d0f26a
Florian Unzicker <unzico@gmail.com>
2020-03-22 14:25:36 +0100

    Remove husky and push everything...

--- types.ts
+++ types.ts
@@ -2,3 +2,13 @@
  * Returns the props of a functional component.
  */
 export type FCProps<T extends (...args: any) => any> = Parameters<T>[0];
+
+/**
+ * Type can be T or null.
+ */
+export type Maybe<T> = T | null;
+
+/**
+ * Returns all unique value types of T.
+ */
+export type ValueOf<T> = T[keyof T];
